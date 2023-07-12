2324b95d0e474b61670274ea5df78525d2a4431d
Sukhdip Singh <121911679+SukhSingh96@users.noreply.github.com>
2023-07-11 21:39:03 -0400

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 import SignUp from './SignUp';
 import Login from './Login';
 import Home from './Home';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <>
     <Router>
-      <Routes>
-        
+    <Header/>
+      <Routes>    
           <Route exact path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-        
+          <Route path="/login" element={<Login />} />      
         </Routes>
-        </Router>
-    </>
+      </Router>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 
