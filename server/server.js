ec4713f802ada2c71f7a8ca46bc16bd5a669c77d
TonyAdimey <tadimey1@gmail.com>
2023-07-12 20:12:22 -0400

    finished controllers

--- server/server.js
+++ server/server.js
@@ -23,6 +23,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+
+
 const startApolloServer = async () => {
     await server.start();
     server.applyMiddleware({ app });
